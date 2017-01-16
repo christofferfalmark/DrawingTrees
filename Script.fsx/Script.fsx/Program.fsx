91d42760976b7b48a56d9d6c657fa46abd7ddc3b
s103148 <s103148@student.dtu.dk>
2017-01-16 21:45:01 +0100

    dpoks

--- Script.fsx/Script.fsx/Program.fsx
+++ Script.fsx/Script.fsx/Program.fsx
@@ -25,24 +25,31 @@ open System.IO
 type 'a Tree = Node of ('a * 'a Tree list);;
 type Extent = (float*float) list;;
 
+// moveextent : Extent * float -> Extent
 let moveextent (e : Extent, x) = List.map (fun (p,q) -> (p+x,q+x)) e;;
 
-let movetree (Node((label, x), subtrees), x' : float) = Node((label, x+x'), subtrees)
+// movetree : ('a * float) Tree * float -> ('a * float) Tree
+let movetree (Node((label, x), subtrees), x' : float) = Node((label, x + x'), subtrees)
 
+// merge : ('a * 'b) list * ('a * 'b) list -> ('a * 'b) list
 let rec merge = function 
     | ([], []) -> []
     | (ps, []) -> ps
     | ([], qs) -> qs
     | ((p,_)::ps, (_,q)::qs) -> (p,q)::merge(ps, qs)
 
+// mergelist : ('a * 'b) list list -> ('a * 'b) list
 let mergelist es = List.fold (fun acc e -> merge (acc, e)) [] es ;;
 
-let rmax (p : float, q : float) = if p > q then p else q
+// rmax : float * float -> float
+let rmax (p, q) = if p > q then p else q
 
+// fit : ('a * float) list * (float * 'b) list -> float
 let rec fit = function
     | ((_,p)::ps, (q,_)::qs) -> rmax(fit (ps,qs), p-q + 1.0)
     | (_,_) -> 0.0
 
+// fitlistl : Extent list -> float list
 let fitlistl es = 
     let rec fitlistl' = function
         | (acc, []) -> []
@@ -50,6 +57,7 @@ let fitlistl es =
                             x::fitlistl' (merge (acc, moveextent(e,x)), es)
     fitlistl'([], es)
 
+// fitlistr : Extent list -> float list
 let fitlistr es =
     let rec fitlistr' = function
         | (acc, []) -> []
@@ -57,10 +65,15 @@ let fitlistr es =
                           x::fitlistr' (merge (moveextent(e,x), acc), es)
     List.rev(fitlistr' ([], (List.rev es)))
 
+// mean : float * float -> float
 let mean (x, y) = (x+y) / 2.0
+
+// fitlist Extent list -> float list
 let fitlist es = List.map(fun (x,y) -> mean(x, y)) (List.zip (fitlistl es) (fitlistr es))
 
+// 'a Tree -> ('a * float) Tree
 let design tree = 
+    // design' 'a Tree -> ('a * float) Tree * Extent
     let rec design' (Node(label, subtrees)) =
         let (trees, extents) = List.unzip (List.map(fun s -> design' s) subtrees)
         let positions = fitlist extents
@@ -71,56 +84,52 @@ let design tree =
         (resulttree, resultextent)
     fst(design' tree)
 
-let rec ExpTree = function
-    | N t -> Node("N", [Node(string(t), [])])
-    | B t -> Node("B", [Node(string(t), [])])
-    | Access a ->  Node("Access", [AccessTree a])
-    | Addr a -> Node("Addr", [AccessTree a])
-    | Apply(s, e) -> Node("Apply", [Node(s, [])] @ List.map (fun x -> ExpTree x) e)
+let rec expToTree = function
+    | N n          -> Node(string n, [])
+    | B b          -> Node(string b, [])
+    | Access a     -> Node("Access", [accToTree a])
+    | Addr a       -> Node("Addr", [accToTree a])
+    | Apply(s,exp) -> Node("Apply", [sToTree s] @ List.map(fun e -> expToTree e) exp)
 
-and AccessTree = function   
-    | AVar s -> Node("AVar", [Node(s, [])]) 
-    | AIndex (a, e) -> Node("AIndex", [AccessTree a] @ [ExpTree e])
-    | ADeref e -> Node("ADeref", [ExpTree e])
+and accToTree = function
+    | AVar s -> sToTree s
+    | AIndex(acc,e) -> Node("AIndex", [accToTree acc] @ [expToTree e])
+    | ADeref e -> Node("ADeref", [expToTree e])
 
-and StmTree = function
-    | PrintLn e -> Node("PrintLn", [ExpTree e])
-    | Ass (a, e) -> Node("Ass", [AccessTree a] @ [ExpTree e])
-    | Return(Some(e)) -> Node("Return", [ExpTree e])
+and stmToTree = function
+    | PrintLn e -> Node("Println", [expToTree e])
+    | Alt gc -> Node("Alt", [gcToTree gc])
+    | Do gc -> Node("Do", [gcToTree gc])
+    | Ass(acc,e) -> Node("Ass", [accToTree acc] @ [expToTree e])
+    | Block([], stms) -> Node("Block", stmsToTree stms)
+    | Block(decs, stms) -> Node("Block", decsToTree decs @ stmsToTree stms)
+    | Call(s, exps) -> Node("Call", expsToTree exps)
+    | Return(Some e) -> Node("Return", [expToTree e])
     | Return(None) -> Node("Return", [])
-    | Call(s, e) -> Node("Apply", [Node(s, [])] @ List.map (fun x -> ExpTree x) e)
-    | Block([], stms) -> Node("Block", List.map(fun x -> StmTree x) stms)
-    | Block(decs, stms) -> Node("Block", (List.map(fun x -> StmTree x) stms) @ (List.map(fun x -> DecTree x) decs)) 
-    | Alt (gc) -> Node("Alt", GCTree gc)
-    | Do (gc) -> Node("Do", GCTree gc)
 
-and TypeTree = function
-    | ITyp              -> Node("int", [])
-    | BTyp              -> Node("bool", [])
-    | ATyp(t, _)        -> Node("ATyp", [TypeTree t])
-    | PTyp(t)           -> Node("PTyp", [TypeTree t])
-    | FTyp(tl, Some(t)) -> Node("FTyp", List.map (fun x -> TypeTree x) tl @ [TypeTree t])
-    | FTyp(tl, None) -> Node("FTyp", List.map (fun x -> TypeTree x) tl)
+and decToTree = function
+    | VarDec(typ, s) -> Node("VarDec", [sToTree s] @ [typToTree typ])
+    | FunDec(Some(typ), s, decs, stm) -> Node("FunDec", [sToTree s] @  [typToTree typ] @ decsToTree decs @ [stmToTree stm])
+    | FunDec(None, s, decs, stm) -> Node("ProcDec", [sToTree s] @ decsToTree decs @ [stmToTree stm])
 
-and DecTree = function
-    | VarDec(t,s) -> Node("VarDec", [Node(s, [])] @ [TypeTree t])
-    | FunDec(Some(t),_, decl, stm) -> Node("FunDec", List.map (fun x -> DecTree x) decl @ [StmTree stm])
-    | FunDec(None, _, _, _) -> Node("FunDec", [])
+and typToTree = function
+    | ITyp -> Node("ITyp", [])
+    | BTyp -> Node("BTyp", [])
+    | ATyp(typ,Some i)-> Node("ATyp", [])
+    | ATyp(typ, None)-> Node("ATyp", [])
+    | PTyp typ -> Node("PTyp", [typToTree typ])
+    | FTyp (typs, Some(typ)) -> Node("FTyp", List.map(fun typ -> typToTree typ) typs @ [typToTree typ])
+    | FTyp (typs, None) -> Node("FTyp", List.map(fun typ -> typToTree typ) typs)
 
-and GCTree = function
-    | GC((e:Exp, s:Stm list)::l) -> [Node("GC", [(ExpTree e)] @ (List.map(fun x -> StmTree x) s))]
-    | _ -> []
+and sToTree s = Node("\"" + s + "\"", [])
+and expsToTree exps = List.map(fun exp -> expToTree exp) exps
+and stmsToTree stms = List.map(fun stm -> stmToTree stm) stms
+and decsToTree decs = List.map(fun dec -> decToTree dec) decs
+and gcToTree (GC(gc)) = Node("GC", List.fold(fun r (e,stms) -> (expToTree e)::stmsToTree stms @ r) [] gc)
+and pToTree (P(decs,stms):Program) = Node("P", decsToTree decs @ stmsToTree stms)
 
-and decListToTree = function
-    | []       -> []
-    | d::dl-> (DecTree d) :: decListToTree dl
 
-and stmListToTree = function
-    | []       -> []
-    | s::sl -> (StmTree s) :: stmListToTree sl
-
-and pToTree (P(decs,stms):Program) = Node("P", decListToTree decs @ stmListToTree stms)
-
+// Convert the Tree to a PostScript file.
 let height = 50.0
 let padding = 10.0
 let width = 50.0
@@ -149,27 +158,30 @@ and subTree = function
                                                      printTree level (Node((s, pos+parent), subtree)) +
                                                      subTree(l, level, parent)
 
+// String.concat method:
+let moveToConcat level pos = let x = string((pos*width) + fst root)
+                             let y = string((snd root - (level * height)))
+                             String.concat "" [x; " "; y ; " moveto\n"]
+let printLabelConcat level pos s = String.concat "" [(moveToConcat level pos); "("; s; ") dup stringwidth pop 2 div neg 0 rmoveto show\n"]
+let doActionConcat x y action = String.concat "" [" "; string x; " "; string y; " "; string action; "\n"]
 
-let moveToCon level pos = String.concat "" (seq[string((pos*width) + fst root); " "; string(snd root - (level * height)) ; " moveto\n"])
-let printLabelCon level pos s = String.concat "" (seq[(moveToCon level pos); "("; s; ") dup stringwidth pop 2 div neg 0 rmoveto show\n"])
-let doActionCon x y action = String.concat "" (seq[" "; string x; " "; string y; " "; string action; "\n"])
-
-let rec printTreeCon (level) = function
-    | Node((s, pos), [])        -> String.concat "" (seq["stroke\n"; printLabelCon level pos s])
-    | Node((s, pos), subtrees)  -> String.concat "" (seq["stroke\n"; printLabelCon level pos s; subTreeCon(subtrees, level+1.0, pos)])
-and subTreeCon = function
+let rec printTreeConcat level = function
+    | Node((s, pos), [])        -> String.concat "" ["stroke\n"; printLabelConcat level pos s]
+    | Node((s, pos), subtrees)  -> String.concat "" ["stroke\n"; printLabelConcat level pos s; subTreeConcat(subtrees, level+1.0, pos)]
+and subTreeConcat = function
     | ([], level, pos) -> ""
     | (Node((s, pos), subtree)::l, level, parent)  ->  let (px,py) = coordinate parent (level-1.0)
                                                        let (x, y) = coordinate (pos+parent) level
-                                                       let sq = (seq[doActionCon px (py-padding) "moveto"; doActionCon px (py-20.0) "lineto"; doActionCon x (py-20.0) "lineto"; doAction x (py-30.0) "lineto"; 
-                                                                        printTreeCon level (Node((s, pos+parent), subtree)); subTreeCon (l, level, parent)])
-                                                       String.concat "" sq
+                                                       let strs = [doActionConcat px (py-padding) "moveto"; doActionConcat px (py-20.0) "lineto"; doActionConcat x (py-20.0) "lineto"; doAction x (py-30.0) "lineto"; 
+                                                                   printTreeConcat level (Node((s, pos+parent), subtree)); subTreeConcat (l, level, parent)]
+                                                       String.concat "" strs
 
 let header = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
-let PSFileWrite path tree = File.WriteAllText (path, header + (printTree 1.0 tree) + "\nshowpage");;
-let PSFileWriteCon path tree = File.WriteAllText (path, header + ((printTreeCon 1.0 tree)) + "\nshowpage");;
+
+let writeToPS path tree = File.WriteAllText (path,  header + (printTree 1.0 tree) + "\nshowpage");;
+let writeToPSConcat path tree = File.WriteAllText (path, String.concat "" [header; ((printTreeConcat 1.0 tree)); "\nshowpage"]);;
 #time "on";;
-PSFileWrite "1a_ex0.ps" (design (pToTree (parseFromFile "Ex0.gc")))
+writeToPS "1a_ex0.ps" (design (pToTree (parseFromFile "Ex0.gc")))
 #time "on";;
-PSFileWriteCon "1b_ex0.ps" (design (pToTree (parseFromFile "Ex0.gc")))
+writeToPSConcat "1b_ex0.ps" (design (pToTree (parseFromFile "Ex0.gc")))
 
